c6be257bcbe18898590d998b9908a6a58383b1f0
gravity <your_email@example.com>
2016-08-03 08:12:03 +0200

    Migrate to etc namespace

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 /* jshint ignore:start */
 Package.describe({
-  name: 'ethereum:web3',
+  name: 'etc:web3',
   version: '0.17.0-alpha',
   summary: 'Ethereum JavaScript API, middleware to talk to a ethreum node over RPC',
   git: 'https://github.com/ethereumproject/ethereum.js',
@@ -13,14 +13,10 @@ Npm.depends({
   "xmlhttprequest": "1.7.0"
 });
 
-
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
-
   // api.use('3stack:bignumber@2.0.0', 'client');
-
   api.export(['Web3', 'BigNumber'], ['client', 'server']);
-
   api.addFiles('dist/web3.js', ['client', 'server']);
   api.addFiles('package-init.js', ['client', 'server']);
 });
