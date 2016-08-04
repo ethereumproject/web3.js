c8ec5e41b63f3dea0569a9a14dd0a71af1baed19
gravity <your_email@example.com>
2016-08-04 02:57:02 +0200

    Migration to new namespace

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 /* jshint ignore:start */
 Package.describe({
-  name: 'ethereum:web3',
+  name: 'etc:web3',
   version: '0.17.0-alpha',
   summary: 'Ethereum JavaScript API, middleware to talk to a ethreum node over RPC',
-  git: 'https://github.com/ethereum/ethereum.js',
+  git: 'https://github.com/ethereumproject/ethereum.js',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
